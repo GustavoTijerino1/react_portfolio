2f29614a9bb0d80fa1a05585edee8548777241be
Gustavo Tijerino <busg26@gmail.com>
2022-05-04 22:29:32 -0500

    Updated Body jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 
+
+
 export default function Body() {
+    const nmn = require('./assets/img/nmn.png')
+    const quiz = require('./assets/img/codeQuiz.png')
+    const passWord = require('./assets/img/passWord.png')
+    const gustavo = require('./assets/img/me.jpg')
+    
   return (
       <>
     <figure id="img">
  
         </figure>
         <main>
-            <div id="me" class="gone"> <p id="hidden">Disclaimer: That's Not Me.</p> <img src="./assets/images/goat.jpg" alt="A pic of me"/> </div>
+            <div id="me"> <img src={gustavo} alt="A pic of me"/> </div>
     
                 <section id="aboutme" >
                     <h2> About me</h2>
@@ -20,19 +27,19 @@ export default function Body() {
             <section id="work">
             <h2>Projects</h2>
 
-                <figure  class="brd" >
-                        <p class="abs"> N-M-N</p>
-                        <a href="https://nerds-meet-nerds.herokuapp.com/"target="_blank"rel="noreferrer"> <img id="shrink" src="./assets/images/nerds-meet-nerds.herokuapp.com_.png" alt="Project 2"/></a>
+                <figure  className="brd" >
+                        <p className="abs"> N-M-N</p>
+                        <a href="https://nerds-meet-nerds.herokuapp.com/"target="_blank"rel="noreferrer"> <img id="shrink" src={nmn} alt="Project 2"/></a>
                  </figure>
                <div id="picBox"> 
-                    <figure class="row"  >
-                        <p class="abs">Quizzle Zizzle</p>
-                        <a href="https://gustavotijerino1.github.io/code-quiz/"target="_blank"rel="noreferrer"> <img id="small" src="./assets/images/codeQuiz.png" alt="Timed Quiz"/></a> 
+                    <figure className="row"  >
+                        <p className="abs">Quizzle Zizzle</p>
+                        <a href="https://gustavotijerino1.github.io/code-quiz/"target="_blank"rel="noreferrer"> <img id="small" src={quiz} alt="Timed Quiz"/></a> 
                     </figure>
                 
-                    <figure class="row"  >
-                        <p class="abs">Password Generator</p>
-                        <a href="https://gustavotijerino1.github.io/password-generator/"target="_blank"rel="noreferrer"> <img id="small" src="./assets/images/FinalProduct.png" alt="password-generator"/></a> 
+                    <figure className="row"  >
+                        <p className="abs">Password Generator</p>
+                        <a href="https://gustavotijerino1.github.io/password-generator/"target="_blank"rel="noreferrer"> <img id="small" src={passWord} alt="password-generator"/></a> 
                     </figure>
                 </div>
             </section>
